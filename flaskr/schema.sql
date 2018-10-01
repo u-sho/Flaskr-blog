7635b2215b6974f1b4a89b01b83fed70d46a1c50
u_sho <720187sho@gmail.com>
2018-10-01 18:04:34 +0900

    Update schema.sql
    to use relational DB, add sqlite3 command, and import two packages, create DB.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,6 +1,20 @@
+from __future__ import with_statement
+from contextlib import closing
+from flaskr import init_db
+
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
   title string not null,
   text string not null
 );
+
+sqlite3 /tmp/flaskr.db < schema.sql
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        
+init_db()
